8dd030ebbc91260f5f4b62d4213e05e9337a3dc5
farooq-azam-khan <khanazamfarooq99@gmail.com>
2023-03-12 15:18:12 -0400

    exercise 4 - bin hex oct - parsing as string done

--- scheme_parser_proj/Main.hs
+++ scheme_parser_proj/Main.hs
@@ -8,6 +8,9 @@ data LispVal = Atom String
                 | List [LispVal]
                 | DottedList [LispVal] LispVal 
                 | Number Integer 
+                | BinaryNumber String 
+                | OctNumber String 
+                | HexNumber String 
                 | String String 
                 | Bool Bool 
                 deriving (Show)
@@ -33,19 +36,58 @@ parseAtom = do
                 _ -> Atom atom 
 
 parseNumber :: Parser LispVal 
--- parseNumber = liftM (Number . read) $ many1 digit 
+parseNumber = liftM (Number . read) $ many1 digit 
 {-parseNumber = do 
     opt <- many1 digit 
     return $ (Number . read) opt 
 -}
-parseNumber = many1 digit >>=  (return . Number . read )
+-- parseNumber = many1 digit >>=  (return . Number . read )
+
+
+-- e.g. 010010
+parseBinaryNumber :: Parser LispVal 
+parseBinaryNumber = do 
+    string "#b " 
+    number_as_string <- many1 (oneOf "01")
+    return $ BinaryNumber number_as_string
+
+parseOctNumber :: Parser LispVal 
+parseOctNumber = do 
+    string "#o " 
+    number_as_string <- many1 (oneOf "01234567") 
+    return $ OctNumber number_as_string
+
+parseHexNumber :: Parser LispVal 
+parseHexNumber = do 
+    string "#x "
+    number_as_string <- many1 (oneOf "0123456789abcdefABCDEF") 
+    return $ HexNumber number_as_string
+
+parseBinaryHexOrOct :: Parser LispVal 
+parseBinaryHexOrOct = do 
+    char '#' 
+    which_base <- oneOf "box" 
+    spaces 
+    case which_base of 
+        'b' -> do 
+            number_as_string <- many1 (oneOf "01") 
+            return $ BinaryNumber number_as_string
+        'o' -> do 
+            number_as_string <- many1 (oneOf "01234567") 
+            return $ OctNumber number_as_string
+        'x' -> do 
+            number_as_string <- many1 (oneOf "0123456789abcdefABCDEF") 
+            return $ HexNumber number_as_string
+
+            
 language_name :: String 
 language_name = "lisp" 
 
 scheme_parser :: Parser LispVal 
-scheme_parser = parseAtom 
-                <|> parseString 
-                <|> parseNumber  
+scheme_parser = parseBinaryHexOrOct <|> 
+                parseNumber <|> 
+                parseAtom <|> 
+                parseString 
 
 readExpr :: String -> String 
 readExpr input = case parse scheme_parser language_name input of 
@@ -54,6 +96,7 @@ readExpr input = case parse scheme_parser language_name input of
 
 spaces :: Parser ()
 spaces = skipMany1 space -- could use: lexeme  
+
 main :: IO ()
 main = do 
     (expr:_) <- getArgs 
